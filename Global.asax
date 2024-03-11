0b1632401e5b6970767175baa5279b51c92e1664
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2024-03-12 00:23:25 +0700

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Website_Course_AVG.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Website_Course_AVG.Startup))]
+namespace Website_Course_AVG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
