756e85e8edfb8b43eb5911f3b2f76d012565ef63
Nguyễn Tấn Dũng <ntdvlog1683@gmail.com>
2024-03-20 16:47:34 +0700

    Not ready

---
+++ Controllers/LessonController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website_Course_AVG.Managers;
+using Website_Course_AVG.Models;
+
+namespace Website_Course_AVG.Controllers
+{
+    public class LessonController : Controller
+    {
+        public LessonController() { }
+        public ActionResult Index(int courseId)
+        {
+            return View();
+        }
+    }
+}
