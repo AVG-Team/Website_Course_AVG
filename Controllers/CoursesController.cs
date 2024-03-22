6b5cbc108fb46c0e6c6cd6e1eb1e6ceb30f42b6b
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2024-03-22 18:21:10 +0700

    feat: detail course

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Website_Course_AVG.Controllers
 {
@@ -10,6 +6,11 @@ namespace Website_Course_AVG.Controllers
     {
         // GET: Courses
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
         {
             return View();
         }
