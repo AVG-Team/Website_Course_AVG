27d2a7e7ce2c87dd7d05f94903a99610db64661d
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2024-03-27 01:22:48 +0700

    feat : page list

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,13 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using PagedList;
+using System.Linq;
+using System.Web.Mvc;
+using Website_Course_AVG.Models;
 
 namespace Website_Course_AVG.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly MyDataDataContext _data = new MyDataDataContext();
         // GET: Courses
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            return View();
+            if (page == null)
+            {
+                page = 1;
+            }
+            var courses = _data.courses.Where(m => m.deleted_at == null).ToList();
+
+            int pageNumber = (page ?? 1);
+
+            return View(courses.ToPagedList(pageNumber, 12));
         }
 
         public ActionResult Details()
---
+++ Models/CourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PagedList;
+using System.Collections.Generic;
+
+namespace Website_Course_AVG.Models
+{
+    public class CourseViewModel
+    {
+        public List<IPagedList> Courses { get; set; }
+
+
+    }
+}
