71a8ce087978e8be38f41bf506b3dde8c4adaa7b
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2024-03-29 12:51:34 +0700

    feat: layout admin

---
+++ Models/AdminViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Website_Course_AVG.Models
+{
+    public class AdminViewModels
+    {
+        public List<course> Courses { get; set; }
+        public List<category> Categories { get; set; }
+        public List<user> Users { get; set; }
+        public List<lesson> Lessons { get; set; }
+        public List<comment> Comments { get; set; }
+        public List<contact> Contacts { get; set; }
+        public List<exercise> Exercises { get; set; }
+        public List<option> Options { get; set; }
+        public List<order> Orders { get; set; }
+        public List<promotion> Promotions { get; set; }
+    }
+}
