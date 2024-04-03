ff5e1b7ec80c84e98994bceee1dd1264af8aa66e
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2024-04-03 15:43:27 +0700

    feat: course

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Website_Course_AVG.Models;
 
 namespace Website_Course_AVG.Controllers
 {
     public class AdminController : Controller
     {
+        private MyDataDataContext _data;
+        public AdminController(MyDataDataContext data)
+        {
+            _data = data;
+        }
         // GET: Admin
         public ActionResult Index()
         {
             return View();
+        }
+
+        // GET : Course 
+        public ActionResult Course()
+        {
+            var courses = _data.courses.ToList();
+
+            AdminViewModels adminView = new AdminViewModels()
+            {
+                Courses = courses
+            };
+
+            return View(adminView);
         }
     }
 }
