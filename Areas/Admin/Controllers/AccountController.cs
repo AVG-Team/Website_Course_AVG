6fad2a72a167d82010fe38d7c3f1e1367a400c0d
Huynguyen <nguyenmaibaohuy2003@gmail.com>
2024-04-09 01:00:25 +0700

    feat: login + register

--- Areas/Admin/Controllers/AccountController.cs
+++ Areas/Admin/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Website_Course_AVG.Managers;
@@ -11,6 +14,45 @@ namespace Website_Course_AVG.Areas.Admin.Controllers
     public class AccountController : Controller
     {
         private readonly MyDataDataContext _data = new MyDataDataContext();
+        private ApplicationUserManager _userManager;
+        private ApplicationSignInManager _signInManager;
+
+        public AccountController()
+        {
+            
+        }
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+
         // GET: Admin/Account
         public ActionResult Index()
         {
@@ -41,17 +83,82 @@ namespace Website_Course_AVG.Areas.Admin.Controllers
 
                 if (account == null)
                 {
-                    Helpers.AddCookie("Error", "Username is not exist!!");
+                    Helpers.AddCookie("Error", "Username and password are wrong !!!");
                     return View(viewModel);
                 }
-                
+
+                var user = account.users.FirstOrDefault();
+                if (user == null)
+                {
+                    Helpers.AddCookie("Error", "Username and password are wrong !!!");
+                    return View(viewModel);
+                }
+                if (user.role > 1)
+                {
+                    userManager.login(account.username);
+                    Helpers.AddCookie("Notify", "Login Successfull");
+                    return RedirectToAction("Index", "Admin");
+                }
+                Helpers.AddCookie("Error", "You are not admin");
+                return View(viewModel);
+
+
             }
-            return View();
+
+            Helpers.AddCookie("Error", "Error Unknow, Please Try Again");
+            return View(viewModel);
         }
 
+        [AllowAnonymous]
         public ActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult>  Register(RegisterViewModel viewModel)
+        {
+            UserManager userManager = new UserManager();
+            if (ModelState.IsValid)
+            {
+                var account = new account
+                {
+                    username = viewModel.userName,
+                    password = viewModel.Password,
+                    created_at = DateTime.Now,
+                    updated_at = DateTime.Now
+                };
+                var result = await userManager.CreateAccountUserAsync(viewModel.userName, account, viewModel.Email);
+                if (result.Succeeded)
+                {
+                    Helpers.AddCookie("Notify", "Register Successfull");
+                    userManager.login(viewModel.userName);
+                    return RedirectToAction("Login", "Account");
+                }
+                AddErrors(result);
+            }
+            Helpers.AddCookie("Error", "Error Unknow, Please Try Again");
+            return View(viewModel);
+        }
+
+
+        public ActionResult Logout()
+        {
+            var UserManager = new UserManager();
+            UserManager.logout();
+
+            Helpers.AddCookie("Notify", "Logout Successful", 5);
+            return RedirectToAction("Index", "Admin");
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
         }
     }
 }
--- Content/vendor/toastify/custom.js
+++ Content/vendor/toastify/custom.js
@@ -1,12 +1,45 @@
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
-Toastify({
-  text: "Welcome to Unify Admin",
-  duration: 3000,
-  close: true,
-  avatar: 'assets/images/user3.png',
-  className: "bg-success",
-  offset: {
-    x: 9,
-    y: 57,
-  },
-}).showToast();
+document.addEventListener("DOMContentLoaded", () => {
+
+    var cookieNotify = getCookie('Notify');
+    if (cookieNotify !== "") {
+        Toastify({
+            text: cookieNotify,
+            duration: 3000,
+            close: true,
+            offset: {
+                x: 9,
+                y: 57,
+            },
+        }).showToast();
+    }
+    var cookieError = getCookie('Error');
+    if (cookieError !== "") {
+        Toastify({
+            text: cookieError,
+            duration: 3000,
+            close: true,
+            offset: {
+                x: 9,
+                y: 57,
+            },
+        }).showToast();
+    }
+});
+
+
