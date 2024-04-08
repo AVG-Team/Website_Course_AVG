a532890a70eeca03f870fbaa2e4f3654fb285fad
Hoàng <vanhoang1231234@gmail.com>
2024-04-08 13:43:22 +0700

    init

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website_Course_AVG.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /*public ActionResult PageNotFound()
+        {
+            Response.StatusCode = 404;
+            ViewBag.ErrorCode = "404";
+            return View();
+        }
+        public ActionResult BadRequest()
+        {
+            Response.StatusCode = 400;
+            ViewBag.ErrorCode = "400";
+            return View();
+        }*/
+        public ActionResult NotFound()
+        {
+            ActionResult result;
+
+            object model = Request.Url.PathAndQuery;
+
+            if (!Request.IsAjaxRequest())
+                result = View(model);
+            else
+                result = PartialView("_NotFound", model);
+
+            return result;
+        }
+    }
+}
