6387063dcb00d269e0e4a85a4c91645194589054
Adonis <tgbao2306@gmail.com>
2024-04-10 12:47:41 +0700

    feat: ckfinder + imgur api + admin/image

--- Areas/Admin/Controllers/CourseController.cs
+++ Areas/Admin/Controllers/CourseController.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Imgur.API.Authentication;
+using Imgur.API.Endpoints;
+using Imgur.API.Models;
 using PagedList;
 using Website_Course_AVG.Areas.Admin.Data.ViewModels;
+using Website_Course_AVG.Managers;
 using Website_Course_AVG.Models;
 
 namespace Website_Course_AVG.Areas.Admin.Controllers
@@ -31,7 +38,6 @@ namespace Website_Course_AVG.Areas.Admin.Controllers
 
             return View(adminView);
         }
-        
 
         public ActionResult Insert()
         {
@@ -46,6 +52,100 @@ namespace Website_Course_AVG.Areas.Admin.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Insert(IEnumerable<HttpPostedFileBase> files, HttpPostedFileBase file, AdminViewModels models)
+        {
+            try
+            {
+                var apiClient = new ApiClient("6efaec52e38d148", "5de13ec766f236d3d39808cb21fec395962922cf");
+                var httpClient = new HttpClient();
+
+                var oAuth2Endpoint = new OAuth2Endpoint(apiClient, httpClient);
+                var authUrl = oAuth2Endpoint.GetAuthorizationUrl();
+
+                var token = new OAuth2Token
+                {
+                    AccessToken = "4e5b5d07a81334ea5c5459dc5c1ef63458c296eb",
+                    RefreshToken = "6e653d2f3b7c99cb1c135f690c5b297b8a1555b1",
+                    AccountId = 180393165,
+                    AccountUsername = "lvxadoniss1",
+                    ExpiresIn = 315360000,
+                    TokenType = "bearer"
+                };
+
+                apiClient.SetOAuth2Token(token);
+                var imageEndpoint = new ImageEndpoint(apiClient, httpClient);
+                var code = "AVG_IMG_" + Helpers.GenerateRandomString(8);
+                string bannerImageUrl = null;
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var bannerStream = file.InputStream;
+                    var bannerUpload = await imageEndpoint.UploadImageAsync(bannerStream);
+                    bannerImageUrl = bannerUpload.Link;
+
+                    var newBanner = new image
+                    {
+                        image1 = bannerImageUrl,
+                        type = true,
+                        category = false,
+                        code = code,
+                    };
+                    _data.images.InsertOnSubmit(newBanner);
+                    _data.SubmitChanges();
+                }
+
+                foreach (var f in files)
+                {
+                    if (f != null && f.ContentLength > 0)
+                    {
+                        var fileStream = f.InputStream;
+                        var imageUpload = await imageEndpoint.UploadImageAsync(fileStream);
+                        var imageUrl = imageUpload.Link;
+
+                        var newImage = new image
+                        {
+                            image1 = imageUrl,
+                            type = true,
+                            category = true,
+                            code = code,
+                        };
+                        _data.images.InsertOnSubmit(newImage);
+                    }
+                }
+
+                var course = new course()
+                {
+                    title = models.Course.title,
+                    description = models.Course.description,
+                    price = models.Course.price,
+                    author = models.Course.author,
+                    category_id = models.Course.category_id,
+                    image_code = code,
+                    created_at = DateTime.Now,
+                };
+                _data.courses.InsertOnSubmit(course);
+
+                // Lưu thay đổi vào cơ sở dữ liệu
+                _data.SubmitChanges();
+
+
+                ViewBag.Message = "Images uploaded successfully!";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, "Error");
+                var categories = _data.categories.Where(cate => cate.deleted_at == null).ToList();
+                var adminView = new AdminViewModels()
+                {
+                    Categories = categories
+                };
+                return View(adminView);
+            }
+        }
+
+        /*[HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Insert(AdminViewModels models)
         {
@@ -68,20 +168,20 @@ namespace Website_Course_AVG.Areas.Admin.Controllers
             }
 
             return RedirectToAction("Index");
-        }
+        }*/
 
         public ActionResult Update(int? id)
         {
             var course = _data.courses.FirstOrDefault(c => c.id == id);
-            var categories = _data.categories.Where( c => c.deleted_at == null ).ToList();
-            var adminView= new AdminViewModels()
+            var categories = _data.categories.Where(c => c.deleted_at == null).ToList();
+            var adminView = new AdminViewModels()
             {
                 Course = course,
                 Categories = categories
             };
             return View(adminView);
         }
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Update(FormCollection form, int? id)
@@ -159,6 +259,5 @@ namespace Website_Course_AVG.Areas.Admin.Controllers
 
             }
         }
-
     }
 }
